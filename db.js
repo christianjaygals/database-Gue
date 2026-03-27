b99a2002e91a6650208742b68b460e2818a61a4e
christianjaygals <christianjaygals@gmail.com>
2026-03-27 21:58:38 +0800

    mao ning new

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql= require("mysql2");
 
 const connection = mysql.createConnection({
     host: "127.0.0.1",
-    port: 3307,
+    port: 3306,
     user: "root",
-    password: "root",
-    database: "weallowsdb"
+    password: "root1",
+    database: "hospitalDATA"
 });
 
 connection.connect((err) => {
